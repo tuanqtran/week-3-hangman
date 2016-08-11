fea5d40261927d9653a46e89cd7ef11110131884
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-11 16:14:27 -0500

    Updated Dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,17 +5,27 @@ var wins = 0;
 var losses = 0;
 var guesses = 5; //Change to 20 in the future
 var guessesSoFar = [];
+var dashAry= [];
 
 // Computer chooses a random word from the array and locks that word until we win or lose and start over.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 // alert(computerGuess);
 var dashRef = document.getElementById("dashes");
 
+
+	// for (i =0; i < computerGuess.length; i++){
+	// 	dashAry[i] = "_";
+	// }
+
     for (i=0; i < computerGuess.length; i++){
     	dashRef.innerHTML = dashRef.innerHTML + "_" + " ";
+
     	// console.log(computerGuess[i]);
     } // Main for loop
 
+    	dashAry.push(dashRef.innerHTML);
+    	console.log(dashAry);
+    	
 document.onkeyup = function(event){
 // User presses a key to guess the word chosen by the computer.
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
@@ -35,7 +45,7 @@ document.onkeyup = function(event){
 	"<p>Wins: " + wins + "</p>" +
 	"<p>Losses: " + losses + "</p>" +
 	"<p>Current Word</p>" +
-	"<p>" + dashRef.innerHTML + "</p>" +
+	"<p>" + dashAry + "</p>" +
 	"<p>Number of Guesses Remaining</p>" +
 	"<p>" + guesses + "</p>" +
 	"<p>Letters Already Guessed</p>" +
