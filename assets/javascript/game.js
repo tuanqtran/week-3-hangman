45abd4ac0efda01f46303e00432925c7720efa0d
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-12 10:03:04 -0500

    Dashes Added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,9 +8,23 @@ var guessesSoFar = [];
 var dashAry= [];
 
 // Computer chooses a random word from the array and locks that word until we win or lose and start over.
+   var html = "<p>Press any key to get started!</p>" +
+	"<p>Wins: " + wins + "</p>" +
+	"<p>Losses: " + losses + "</p>" +
+	"<p>Current Word</p>" +
+	"<p>" + dashAry + "</p>" +
+	"<p>Number of Guesses Remaining</p>" +
+	"<p>" + guesses + "</p>" +
+	"<p>Letters Already Guessed</p>" +
+	"<p>" + guessesSoFar + "</p>";
+
+	document.querySelector(".hangman").innerHTML = html;
+
+
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+document.onkeyup = function(event){
 // alert(computerGuess);
-var dashRef = document.getElementById("dashes");
+// var dashRef = document.getElementById("dashes");
 
 
 	// for (i =0; i < computerGuess.length; i++){
@@ -18,28 +32,39 @@ var dashRef = document.getElementById("dashes");
 	// }
 
     for (i=0; i < computerGuess.length; i++){
-    	dashRef.innerHTML = dashRef.innerHTML + "_" + " ";
-
+    	dashAry.push(" _ ");
+    	// dashRef.innerHTML = dashRef.innerHTML + "_" + " ";
     	// console.log(computerGuess[i]);
     } // Main for loop
 
-    	dashAry.push(dashRef.innerHTML);
+    	// dashAry.push(dashRef.innerHTML);
     	console.log(dashAry);
-    	
-document.onkeyup = function(event){
+
+
 // User presses a key to guess the word chosen by the computer.
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     guessesSoFar.push(userGuess);
 
 // Compare userGuess letter to computerGuess word.
-	for (i=0; i < computerGuess.length; i++){
-			if (userGuess == computerGuess[i]){
-				alert(userGuess);
-			}
-	}
+	// for (i=0; i < computerGuess.length; i++){
+	// 		if (userGuess == computerGuess[i]){
+	// 			alert(userGuess);
+	// 		}
+	// }
 
+var tempAry = [];
+	for (var k = 0; k < computerGuess.length; k++) {
+ 		if (computerGuess[k] === userGuess) {
+ 			tempAry[k] = userGuess;
+ 			console.log(tempAry);
+ 		}else{
+ 			tempAry[k] = dashAry[k];
+ 		}
 
+ 		// if (tempAry[k].indexof());
+ 	}
 
+ 	dashAry = tempAry;
 
    var html = "<p>Press any key to get started!</p>" +
 	"<p>Wins: " + wins + "</p>" +
